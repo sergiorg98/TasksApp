5be02e99b92f8879d91d62fd90f3950d7f4fd42e
SergioRG <sergioramiro98@gmail.com>
2024-05-06 23:09:59 +0200

    breate backend task api

---
+++ backend/tasks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
